d04f5d7f5eb7ba21bd6bcc76de9e6f327525bb34
Donovan Clarke <donovanclarketx@gmail.com>
2015-10-29 18:11:14 -0400

    small changes

---
+++ db/migrate/20151029200011_remove_image_from_products.rb
@@ -0,0 +1,8 @@
+class RemoveImageFromProducts < ActiveRecord::Migration
+  def change
+    remove_column :products, :image_file_name, :string
+    remove_column :products, :image_content_type, :string
+    remove_column :products, :image_file_size, :integer
+    remove_column :products, :image_updated_at, :string
+  end
+end

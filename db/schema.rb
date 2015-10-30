49f371bd39ee4e8758e96f3c61d85b4b7cb8b084
Donovan Clarke <donovanclarketx@gmail.com>
2015-10-29 20:16:37 -0400

    more fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029225041) do
+ActiveRecord::Schema.define(version: 20151030001605) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -37,11 +37,12 @@ ActiveRecord::Schema.define(version: 20151029225041) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.string   "image_url"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
     t.text     "description"
-    t.decimal  "price",       precision: 8, scale: 2
+    t.decimal  "price",           precision: 8, scale: 2
     t.string   "picture"
+    t.string   "image_file_name"
   end
 
   create_table "users", force: :cascade do |t|

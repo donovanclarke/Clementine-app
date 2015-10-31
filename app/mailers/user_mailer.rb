31554cf73239ef9f61e679ca0a2259691e3141b7
Donovan Clarke <donovanclarketx@gmail.com>
2015-10-30 21:35:16 -0400

    added feature, user receives automated email upon sign up.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,11 @@ class UserMailer < ApplicationMailer
     :to => 'donovan@donovanclarke.com',
     :subject => "A new contact form message from #{name}")
   end
+
+  def welcome_email(user)
+    @user = user
+    mail(:from =>'service@clementinecourier.com',
+      :to => "#{user.email}",
+      :subject => "Welcome. Thank you for registering!")
+  end
 end
